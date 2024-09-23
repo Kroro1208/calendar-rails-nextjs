14d94069dda52ffc0d55496fe51269cbd0aa487f
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-23 13:45:56 +0900

    chacraUI設定して必要なページ作成した

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Providers } from "./components/provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,8 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><Providers>
         {children}
+      </Providers>
       </body>
     </html>
   );
